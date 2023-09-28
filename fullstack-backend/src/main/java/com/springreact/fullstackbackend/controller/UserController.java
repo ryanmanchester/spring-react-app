3cca5fe068e8ffee595c1d165da56f906197acfc
Ryan Manchester <ryanm17@gmail.com>
2023-09-28 14:38:34 -0400

    connects spring boot backend to react frontend via cross origins annotation

--- fullstack-backend/src/main/java/com/springreact/fullstackbackend/controller/UserController.java
+++ fullstack-backend/src/main/java/com/springreact/fullstackbackend/controller/UserController.java
@@ -3,6 +3,7 @@ package com.springreact.fullstackbackend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import com.springreact.fullstackbackend.model.User;
 import com.springreact.fullstackbackend.repository.UserRepository;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 	
 	@Autowired
