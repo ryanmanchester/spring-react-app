180c9a62d292ce39a036f906633106b0c1441de9
Ryan Manchester <ryanm17@gmail.com>
2023-09-28 12:08:56 -0400

    implements post mapping on user controller

--- fullstack-backend/src/main/java/com/springreact/fullstackbackend/controller/UserController.java
+++ fullstack-backend/src/main/java/com/springreact/fullstackbackend/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.springreact.fullstackbackend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springreact.fullstackbackend.model.User;
 import com.springreact.fullstackbackend.repository.UserRepository;
 
 @RestController
@@ -11,5 +14,9 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@PostMapping("/user")
+	User newUser(@RequestBody User newUser) {
+		return userRepository.save(newUser);
+	}
 
 }
