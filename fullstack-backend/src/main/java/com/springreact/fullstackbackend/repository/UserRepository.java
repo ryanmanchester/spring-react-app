6aa2a32e29ea64353a9b050fc11a30de84719f4d
Ryan Manchester <ryanm17@gmail.com>
2023-09-28 11:52:14 -0400

    test commit

--- fullstack-backend/src/main/java/com/springreact/fullstackbackend/repository/UserRepository.java
+++ fullstack-backend/src/main/java/com/springreact/fullstackbackend/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.springreact.fullstackbackend.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	public void getRepo();
 }
