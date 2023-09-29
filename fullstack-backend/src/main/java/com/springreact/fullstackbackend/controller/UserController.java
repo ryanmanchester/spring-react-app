c153da188ae3d82d86d965f36faa6fee681066e6
Ryan Manchester <ryanm17@gmail.com>
2023-09-29 11:57:07 -0400

    implements delete mapping on user controller

--- fullstack-backend/src/main/java/com/springreact/fullstackbackend/controller/UserController.java
+++ fullstack-backend/src/main/java/com/springreact/fullstackbackend/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,6 +52,11 @@ public class UserController {
 					user.setEmail(newUser.getEmail());
 					return userRepository.save(user);
 				}).orElseThrow(() -> new UserNotFoundException(id)); 
+	}
+	
+	@DeleteMapping("/users/{id}")
+	void deleteUser(@PathVariable Long id) {
+		 userRepository.deleteById(id);
 	}
 	
 
