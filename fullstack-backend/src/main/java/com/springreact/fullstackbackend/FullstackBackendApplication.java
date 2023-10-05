a31a80e01e5eb21492b0ccb5ab16842d626186ba
Ryan Manchester <ryanm17@gmail.com>
2023-10-05 12:51:21 -0400

    converts packaging to war file for deployment to heroku

--- fullstack-backend/src/main/java/com/springreact/fullstackbackend/FullstackBackendApplication.java
+++ fullstack-backend/src/main/java/com/springreact/fullstackbackend/FullstackBackendApplication.java
@@ -2,10 +2,17 @@ package com.springreact.fullstackbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class FullstackBackendApplication{
+public class FullstackBackendApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FullstackBackendApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(FullstackBackendApplication.class, args);
